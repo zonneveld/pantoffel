5ae58e1c6d5ff9c9b0e0b4d34f3a49a1573349bf
zonneveld <zdj2606@novacollege.nl>
2024-10-19 23:10:56 +0200

    erg..

--- pantoffel_game/game.py
+++ pantoffel_game/game.py
@@ -15,63 +15,32 @@ x_pulse = 0
 y_pulse = 0
 z_pulse = 0
 
-
-
 on_device = False
 
-if platform.system() == 'Linux':
-   # import RPi.GPIO as GPIO #<-- vervangen met pizero!
-   from gpiozero import RotaryEncoder      
-   x_encoder = RotaryEncoder(X_ENC1,X_ENC2)
-   
-   def x_inc_ev(value):
-      global x_pulse
-      x_pulse+=value
-      print(f"x_pulse:{x_pulse}")
+def x_inc_ev(value):
+   global x_pulse
+   x_pulse+=value
+   print(f"x_pulse:{x_pulse}")
 
-   def system_start():
-      global x_pulse,x_encoder
-
-      x_encoder.when_rotated_clockwise          = lambda : x_inc_ev(1)
-      x_encoder.when_rotated_counter_clockwise  = lambda : x_inc_ev(-1)
-      
-
-      y_encoder = RotaryEncoder(Y_ENC1,Y_ENC2)
-      
-      
-      z_encoder = RotaryEncoder(Z_ENC1,Z_ENC2)
-      
-      pass
-   def screen_setup():
-      return pygame.display.set_mode((0, 0),pygame.FULLSCREEN )      
-
-   def system_end():
-      pass
-      # GPIO.cleanup() 
+#linux:
+from gpiozero import RotaryEncoder      
+x_encoder = RotaryEncoder(X_ENC1,X_ENC2)
+x_encoder.when_rotated_clockwise          = lambda : x_inc_ev(1)
+x_encoder.when_rotated_counter_clockwise  = lambda : x_inc_ev(-1)
 
 
+# elif platform.system() == 'Windows':
+#    def system_start():
+#       pass
+#    def system_end():
+#       pass
+#    def screen_setup():
+#       return pygame.display.set_mode([640, 640])
 
-elif platform.system() == 'Windows':
-   def system_start():
-      pass
-   def system_end():
-      pass
-   def screen_setup():
-      return pygame.display.set_mode([640, 640])
-
-
-
-
-system_start()
 
 pygame.init()
-
-# screen = pygame.display.set_mode([640, 640])
-
-screen = screen_setup()
-
+screen = pygame.display.set_mode((0, 0),pygame.FULLSCREEN )
 pygame.mouse.set_visible(False) 
-
 pygame.display.set_caption("Hello World")
 
 [w,h] = pygame.display.get_window_size()
